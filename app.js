ff36fd9c36208fc07279d8e6d9e4e910406d7590
Joe Valentine <joe.valentine@live.co.uk>
2022-03-17 10:34:21 +0000

    use express/cors test

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 const cors = require("cors");
-app.use(cors());
 
 app.use(express.json());
+app.use(cors());
 
 const { getEndpoints } = require("./controllers/controllers.js");
 
